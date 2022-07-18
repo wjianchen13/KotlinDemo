cd2f60d6f75fe840d1af2333b522d9fd8dfb568f
1812821235 <1812821235@qq.com>
2022-07-19 07:47:52 +0800

    变量

--- app/src/main/java/com/example/kotlin/normal/KotlinActivity.java
+++ app/src/main/java/com/example/kotlin/normal/KotlinActivity.java
@@ -1,35 +1,32 @@
-package com.example.kotlin;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.kotlin.normal;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.kotlin.jetpack.JetpackActivity;
-import com.example.kotlin.jetpack.lifecycle.LifeCycleActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kotlin.R;
 import com.example.kotlin.jetpack.livedata.LiveDataActivity;
 import com.example.kotlin.jetpack.viewmodel.ViewModelActivity;
 import com.example.kotlin.jetpack.viewmodel.ViewModelActivity2;
-import com.example.kotlin.normal.KotlinActivity;
+import com.example.kotlin.normal.var1.VarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class KotlinActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_kotlin);
     }
 
-    public void onKotlin(View v) {
-        startActivity(new Intent(this, KotlinActivity.class));
+    public void onVar(View v) {
+        startActivity(new Intent(this, VarActivity.class));
     }
 
-    public void onJetpack(View v) {
-        startActivity(new Intent(this, JetpackActivity.class));
+    public void onViewModel(View v) {
+        startActivity(new Intent(this, ViewModelActivity.class));
     }
-
-
 
     public void onViewModel2(View v) {
         startActivity(new Intent(this, ViewModelActivity2.class));
